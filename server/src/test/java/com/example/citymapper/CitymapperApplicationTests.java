d86890ca5e2616555668a36844ed1029deb41b76
lam-vincent <mrccniv@gmail.com>
2024-05-30 23:08:46 +0200

    add: React and Spring Boot  Basic project Configuration

---
+++ server/src/test/java/com/example/citymapper/CitymapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.citymapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitymapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

3d72a4d870b7a4ba933ef9c6b569b6602a28a3e2
P4UL-M <paul.mairesse@free.fr>
2024-06-04 10:35:23 +0200

    Diesel + Rover init

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,36 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket::response::stream::TextStream;
+use rocket::tokio::time::{interval, Duration};
+use rocket::Request;
+
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+/// Produce an infinite series of `"hello"`s, one per second.
+#[get("/infinite-hellos")]
+fn hello() -> TextStream![&'static str] {
+    TextStream! {
+        let mut interval = interval(Duration::from_secs(1));
+        loop {
+            yield "hello";
+            interval.tick().await;
+        }
+    }
+}
+
+#[catch(404)]
+fn not_found(req: &Request) -> String {
+    format!("Sorry, '{}' is not a valid path.", req.uri())
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![hello])
+        .register("/", catchers![not_found])
 }

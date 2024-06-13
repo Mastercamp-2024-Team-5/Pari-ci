41b33d351f93a9c770761d1bd41117a3aabc1d6d
Looki-fr <lemeilllouis@gmail.com>
2024-06-13 12:05:00 +0200

    corrected responsive home

--- client/src/components/LeftSearch.tsx
+++ client/src/components/LeftSearch.tsx
@@ -8,35 +8,46 @@ import {
   InputGroup,
   Button,
   Flex,
+  Text
 } from "@chakra-ui/react";
 import Icon from "./Icon";
 import useScreenWidth from "./useScreenWidth";
 import { useState } from "react";
 
-const LeftSearch = () => {
+const LeftSearch = (
+  {
+    departure,
+    setDeparture,
+    destination,
+    setDestination,
+    startAt,
+    setStartAt,
+    endAt,
+    setEndAt
+  }: {
+    departure: string,
+    setDeparture: React.Dispatch<React.SetStateAction<string>>,
+    destination: string,
+    setDestination: React.Dispatch<React.SetStateAction<string>>,
+    startAt: string,
+    setStartAt: React.Dispatch<React.SetStateAction<string>>,
+    endAt: string,
+    setEndAt: React.Dispatch<React.SetStateAction<string>>
+  }
+) => {
   const screenWidth = useScreenWidth();
-  const [departure, setDeparture] = useState('');
-  const [destination, setDestination] = useState('');
-  const [startAt, setStartAt] = useState('');
-  const [endAt, setEndAt] = useState('');
+
 
   return (
     <Center>
       <Stack spacing={8} w="100%">
         <Stack align="center">
-          <Heading fontFamily="Karla" marginTop="15%" fontSize="4xl">
-            Citymapper
+          <Heading fontFamily="Karla" fontWeight="600" marginTop={screenWidth<600?"30%":"15%"} fontSize={screenWidth<600?"5xl":"4xl"} marginBottom='5%'>
+            CITYMAPPER
           </Heading>
-          {/* <Text
-            fontFamily="Karla"
-            fontSize="lg"
-            color="32403B"
-            mb={4}
-            marginBottom="15%"
-          > 
-            Subtitle
-          </Text> */}
-          <Icon item="barre" color="a" />
+          <Text fontSize={screenWidth<600?"xl":"lg"} marginBottom={screenWidth<600?'15%':'10%'}>
+            Trouvez votre itinéraire
+          </Text>
         </Stack>
         <VStack spacing={15} w="90%" alignSelf="center">
           <Input
@@ -48,11 +59,13 @@ const LeftSearch = () => {
             rounded="md"
             type="text"
             placeholder="Départ"
-            fontSize="lg"
-            p={8}
+            fontSize={screenWidth<600?"xl":"lg"}
+            p={screenWidth<600?10:8}
             bg="white"
+            borderRadius="15"
             value={departure}
             onChange={(e) => setDeparture(e.target.value)}
+            marginBottom={screenWidth<600?'5%':'0%'}
           />
           <Input
             focusBorderColor="#5eaf91"
@@ -63,11 +76,13 @@ const LeftSearch = () => {
             rounded="md"
             type="text"
             placeholder="Destination"
-            fontSize="lg"
-            p={8}
+            fontSize={screenWidth<600?"xl":"lg"}
+            p={screenWidth<600?10:8}
             bg="white"
+            borderRadius="15"
             value={destination}
             onChange={(e) => setDestination(e.target.value)}
+            marginBottom={screenWidth<600?'5%':'0%'}
           />
           <Flex maxW={"100%"} margin={0} padding={0} display={"flex"} flex={1} direction={"row"} align="center" justify="center">
             <Input
@@ -79,10 +94,11 @@ const LeftSearch = () => {
               rounded="md"
               type="text"
               placeholder="Partir à"
-              fontSize="lg"
-              p={8}
+              fontSize={screenWidth<600?"xl":"lg"}
+              p={screenWidth<600?10:8}
               bg="white"
               width="48%"
+              borderRadius="15"
               marginRight={'5%'}
               value={startAt}
               onChange={(e) => setStartAt(e.target.value)}
@@ -95,33 +111,36 @@ const LeftSearch = () => {
               borderColor="gray.200"
               rounded="md"
               type="text"
-              placeholder="Partir à"
-              fontSize="lg"
-              p={8}
+              placeholder="Arriver à"
+              fontSize={screenWidth<600?"xl":"lg"}
+              p={screenWidth<600?10:8}
               bg="white"
               width="48%"
+              borderRadius="15"
               value={endAt}
               onChange={(e) => setEndAt(e.target.value)}
             />
           </Flex>
+          <Button
+            fontFamily="Karla"
+            bg="#84C7AE"
+            color="white"
+            _hover={{
+              bg: "#5eaf91",
+            }}
+            rounded="md"
+            marginBottom={screenWidth<600?'5%':'0%'} 
+            fontSize={screenWidth<600?'3xl':'xl'}
+            whiteSpace="wrap"
+            width={screenWidth<600?"80%":"80%"}
+            alignSelf="center"
+            borderRadius="15"
+            marginTop={screenWidth<600?'15%':'10%'}
+            p={screenWidth<600?12:8} // Added padding
+          >
+            Trouver l’itinéraire
+          </Button>
         </VStack>
-        
-        <Button
-          fontFamily="Karla"
-          bg="#84C7AE"
-          color="white"
-          _hover={{
-            bg: "#5eaf91",
-          }}
-          rounded="md"
-          fontSize="xl"
-          width="60%"
-          alignSelf="center"
-          borderRadius="15"
-          p={8} // Added padding
-        >
-          Trouver l’itinéraire
-        </Button>
       </Stack>
     </Center>
   );

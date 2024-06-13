b36ed52209b79a6236573f48aa6455ff9950f5f5
Looki-fr <lemeilllouis@gmail.com>
2024-06-13 09:54:10 +0200

    accessible screen responsive

--- client/src/components/AccessibleScreen.tsx
+++ client/src/components/AccessibleScreen.tsx
@@ -8,10 +8,12 @@ import {
   Image,
   Text,
   Box,
+  Flex
 } from "@chakra-ui/react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import useScreenWidth from "./useScreenWidth";
 
 // Import images
 import accessibility1 from './../assets/accessibility1.jpg';
@@ -34,21 +36,31 @@ const AccessibleScreen = ({ setAccessibleScreen, setStationAccessibleOnly, stati
     autoplay: true,
     autoplaySpeed: 3000,
   };
+  const screenWidth = useScreenWidth();
 
   // Set width and height for the images
   const width_picture = "100%";
-  const height_picture = "60vh"; // Set a fixed height for the images
+  const height_picture = screenWidth<600?"60vw":"30vw"; // Set a fixed height for the images
+
 
   return (
-    <>
-      <Container flex="1" bg="F6FBF9" maxW="40vw" height="100vh">
+    <Flex 
+      margin={0}  
+      padding={0} 
+      flex="1" 
+      maxH="100vh" 
+      maxW="100vw" 
+      bg="F6FBF9" 
+      direction={screenWidth<600?"column":"row"}
+    >
+      <Container flex="1" maxW={screenWidth<600?"100vw":"40vw"} height={screenWidth<600?"60vh":"100vh"}>
         <Center>
           <Stack spacing={8} w="100%">
             <Stack align="center">
-              <Heading fontFamily="Karla" marginTop="15%" fontSize="4xl" marginBottom={"5%"}>
+              <Heading textAlign={'center'} fontFamily="Karla" marginTop="15%" fontSize="4xl" marginBottom={"5%"}>
                 Citymapper
               </Heading>
-              <Text fontFamily="Karla" fontSize="xl" color="32403B" mb={4} marginBottom="25%">
+              <Text  textAlign={'center'} fontFamily="Karla" fontSize="xl" color="32403B" mb={4} marginBottom={screenWidth<600?"15%":"25%"}>
                 Proposer seulement les stations accessibles ?
               </Text>
             </Stack>
@@ -98,56 +110,56 @@ const AccessibleScreen = ({ setAccessibleScreen, setStationAccessibleOnly, stati
       </Container>
       <Container
         flex="1"
-        maxW="60vw"
-        margin="0 auto"
+        maxW={screenWidth<600?"100vw":"60vw"}
+        margin="0"
         padding={0}
         display="flex"
         alignItems="center"
         justifyContent="center"
-        height="100vh"
+        height={screenWidth<600?"40vh":"100vh"}
       >
-        <Box height="60vh" width="100%" display="flex" alignItems="center" justifyContent="center">
-          <Slider {...settings} style={{ width: "70%", height: "100%" }}>
-            <Box display="flex" justifyContent="center" alignItems="center" height="100%">
-              <Image 
-                src={accessibility1} 
-                alt="Image 1" 
-                style={{ 
-                  width: width_picture, 
-                  height: height_picture, 
-                  objectFit: "contain",  // Ensure the image fits within the container without being cut
-                  objectPosition: "center" // Center the image
-                }} 
-              />
-            </Box>
-            <Box display="flex" justifyContent="center" alignItems="center" height="100%">
-              <Image 
-                src={accessibility2} 
-                alt="Image 2" 
-                style={{ 
-                  width: width_picture, 
-                  height: height_picture, 
-                  objectFit: "contain",  // Ensure the image fits within the container without being cut
-                  objectPosition: "center" // Center the image
-                }} 
-              />
-            </Box>
-            <Box display="flex" justifyContent="center" alignItems="center" height="100%">
-              <Image 
-                src={accessibility3} 
-                alt="Image 3" 
-                style={{ 
-                  width: width_picture, 
-                  height: height_picture, 
-                  objectFit: "contain",  // Ensure the image fits within the container without being cut
-                  objectPosition: "center" // Center the image
-                }} 
-              />
-            </Box>
+      <Box height={screenWidth<600?"40vh":"60vh"} width="100%" display="flex" alignItems="center" justifyContent="center"  margin={0} padding={0}>
+        <Slider {...settings} style={{ width: "70%", height: "100%"}}>
+          <Box display="flex" justifyContent="center" alignItems="center" height="100%" margin={0} padding={0}>
+            <Image 
+              src={accessibility1} 
+              alt="Image 1" 
+              style={{ 
+                width: width_picture, 
+                height: height_picture, 
+                objectFit: "contain",  // Ensure the image fits within the container without being cut
+                objectPosition: "center" // Center the image
+              }} 
+            />
+          </Box>
+          <Box display="flex" justifyContent="center" alignItems="center" height="100%">
+            <Image 
+              src={accessibility2} 
+              alt="Image 2" 
+              style={{ 
+                width: width_picture, 
+                height: height_picture, 
+                objectFit: "contain",  // Ensure the image fits within the container without being cut
+                objectPosition: "center" // Center the image
+              }} 
+            />
+          </Box>
+          <Box display="flex" justifyContent="center" alignItems="center" height="100%">
+            <Image 
+              src={accessibility3} 
+              alt="Image 3" 
+              style={{ 
+                width: width_picture, 
+                height: height_picture, 
+                objectFit: "contain",  // Ensure the image fits within the container without being cut
+                objectPosition: "center" // Center the image
+              }} 
+            />
+          </Box>
           </Slider>
         </Box>
       </Container>
-    </>
+    </Flex>
   );
 };
 

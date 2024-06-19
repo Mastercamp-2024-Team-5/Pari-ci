f800c53eef9001d3f6adfb3176ee42cd3ea703aa
P4UL-M <paul.mairesse@free.fr>
2024-06-19 18:20:46 +0200

    Fix error in down migration

--- server/migrations/2024-06-08-224856_stop_times/down.sql
+++ server/migrations/2024-06-08-224856_stop_times/down.sql
@@ -1,6 +1,6 @@
 -- This file should undo anything in `up.sql`
 DROP TABLE stop_times;
 
-DROP INDEX arrival_time_index;
+DROP INDEX IF EXISTS arrival_time_index;
 
-DROP INDEX departure_time_index;
+DROP INDEX IF EXISTS departure_time_index;

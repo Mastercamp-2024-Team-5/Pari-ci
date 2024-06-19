dc50895cf7a15fb535356bdba8d0696d626cd32f
P4UL-M <paul.mairesse@free.fr>
2024-06-19 19:01:01 +0200

    remove useless index + separate wait time in graph

--- server/migrations/2024-06-12-072417_average_stop_times/up.sql
+++ server/migrations/2024-06-12-072417_average_stop_times/up.sql
@@ -3,14 +3,17 @@ CREATE MATERIALIZED VIEW average_stop_times AS
 SELECT
     st1.stop_id,
     st2.stop_id AS next_stop_id,
+    t.route_id,
     AVG(st2.arrival_time - st1.departure_time)::INT AS avg_travel_time
 FROM
     stop_times as st1
     JOIN stop_times as st2 ON st1.trip_id = st2.trip_id
+    JOIN trips as t ON st1.trip_id = t.trip_id
     AND st1.stop_sequence = st2.stop_sequence - 1
 GROUP BY
     st1.stop_id,
-    st2.stop_id
+    st2.stop_id,
+    t.route_id
 ORDER BY
     avg_travel_time;
 

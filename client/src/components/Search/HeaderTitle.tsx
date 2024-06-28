bbff8b84df72826fa78ee799c4891787388f00e6
Looki-fr <lemeilllouis@gmail.com>
2024-06-28 12:13:14 +0200

    logoooooo

--- client/src/components/Search/HeaderTitle.tsx
+++ client/src/components/Search/HeaderTitle.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Heading, Text } from "@chakra-ui/react";
+import { Heading, Text, Image } from "@chakra-ui/react";
 import useScreenWidth from "../Shared/useScreenWidth.tsx";
-
+import logo from "../../assets/logo.svg";
 
 export function HeaderTitle(): React.ReactElement {
     const screenWidth = useScreenWidth();
 
     return (
         <div>
-            <Heading
-                fontFamily="Karla"
-                fontWeight="650"
+            <Image
+                margin={0}
                 marginTop={screenWidth < 700 ? "10%" : "15%"}
-                fontSize={screenWidth < 700 ? "5xl" : "4xl"}
-            >
-                Pari'ci
-            </Heading>
+                src={logo}
+                alt="logo"
+                width="100px"
+                height="100px"
+                padding={0}
+            />
             <Text fontSize={screenWidth < 700 ? "xl" : "lg"} marginBottom="5%">
                 Trouvez votre itinéraire
             </Text>

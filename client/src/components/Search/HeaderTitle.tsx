d6915772c7efc359b623b4229a2188cdc442f7a8
P4UL-M <paul.mairesse@free.fr>
2024-06-28 16:49:25 +0200

    ESLint

--- client/src/components/Search/HeaderTitle.tsx
+++ client/src/components/Search/HeaderTitle.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import { Heading, Text, Image } from "@chakra-ui/react";
+import { Text, Image } from "@chakra-ui/react";
 import useScreenWidth from "../Shared/useScreenWidth.tsx";
 import logo from "../../assets/logo.svg";
 
 export function HeaderTitle(): React.ReactElement {
-    const screenWidth = useScreenWidth();
+  const screenWidth = useScreenWidth();
 
-    return (
-        <center>
-            <Image
-                margin={0}
-                marginTop={screenWidth < 700 ? "10%" : "15%"}
-                src={logo}
-                alt="logo"
-                width="100px"
-                height="100px"
-                padding={0}
-                marginBottom="5%"
-            />
-            <Text fontSize={screenWidth < 700 ? "xl" : "lg"} marginBottom="5%">
-                C'est Par'Où ?
-            </Text>
-        </center>
-    );
+  return (
+    <center>
+      <Image
+        margin={0}
+        marginTop={screenWidth < 700 ? "10%" : "15%"}
+        src={logo}
+        alt="logo"
+        width="100px"
+        height="100px"
+        padding={0}
+        marginBottom="5%"
+      />
+      <Text fontSize={screenWidth < 700 ? "xl" : "lg"} marginBottom="5%">
+        C'est Par'Où ?
+      </Text>
+    </center>
+  );
 }
